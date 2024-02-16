6d50750c99cf581e7c62d02ce96b33567fe9794d
Ruslan Khamitov <ruslan.khamitov@student.tuke.sk>
2024-02-16 09:43:45 +0100

    init bootstrap react template

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+
+  return (
+    <>
+      <Button>Test</Button>
+    </>
+  )
+}
+
+export default App
